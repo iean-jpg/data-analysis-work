1f0b1e72a3f77c5b3d4a0806d29e6047a6afb381
WhiteWolf <liyue5171@gmail.com>
2021-12-24 09:27:04 +0800

    第一次提交

---
+++ main.py
@@ -0,0 +1,74 @@
+import pandas as pd
+from sklearn import model_selection
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
+train_df = pd.read_csv('./data/train.csv')
+test_df = pd.read_csv('./data/test.csv')
+
+# 解决空值
+train_df.Embarked.fillna(train_df.Embarked.mode()[0], inplace=True)
+train_df.Cabin.fillna('UnKnow', inplace=True)
+# train_df.Age.fillna(train_df.Age.median(), inplace=True)
+
+# 特征选择
+# 提取船舱位置信息
+train_df.loc[:, 'Cabin'] = train_df.Cabin.apply(lambda x: x[0])
+# 船票号获取相同船票号的数量
+tc = dict(train_df.Ticket.value_counts())
+train_df['ticket_group'] = train_df.Ticket.apply(lambda x: tc[x])
+# Name名字拆分提取三部分内容
+train_df['f_name'] = train_df.Name.apply(lambda x: x[:x.find(',')])
+train_df['title'] = train_df.Name.apply(lambda x: x[x.find(',') + 1:x.find('.')])
+train_df['l_name'] = train_df.Name.apply(lambda x: x[x.find('.') + 1:])
+# 增加家庭成员数量
+train_df['f_size'] = train_df.Parch + train_df.SibSp + 1
+# 对家庭成员进行分段
+train_df['f_size_type'] = 'a'
+train_df.loc[(train_df.f_size >= 2) & (train_df.f_size <= 4), 'f_size_type'] = 'b'
+train_df.loc[train_df.f_size > 4, 'f_size_type'] = 'c'
+# 对Age分段
+train_df['age_type'] = 'a'
+train_df.loc[(train_df.Age > 16) & (train_df.Age <= 30), 'age_type'] = 'b'
+train_df.loc[(train_df.Age > 30) & (train_df.Age <= 40), 'age_type'] = 'c'
+train_df.loc[(train_df.Age > 40), 'age_type'] = 'd'
+# Age年龄，缺失值用众数代替
+titles = train_df.title.unique()
+for title in titles:
+    train_df.loc[(train_df.title == title) & (train_df.Age.isnull()), 'Age'] = train_df.Age[
+        train_df.title == title].median()
+# 增加儿童标识
+train_df['is_child'] = 0
+train_df.loc[train_df.Age < 18, 'is_child'] = 1
+# 名字长度
+train_df['name_length'] = train_df.Name.apply(lambda x: len(x))
+# 家族中是否有人遇难或获救
+train_df['family_survived'] = 0.5
+for idx, rows in train_df.groupby(['f_name', 'Fare']):
+    if len(rows) == 1:
+        continue
+    for idx2, row in rows.iterrows():
+        survived = rows.drop(idx2).Survived.max()
+        train_df.loc[(train_df.PassengerId == row.PassengerId), 'family_survived'] = 1
+
+for idx, rows in train_df.groupby(['Ticket']):
+    if len(rows) == 1:
+        continue
+    for idx2, row in rows.iterrows():
+        survived = rows.drop(idx2).Survived.max()
+        train_df.loc[(train_df.PassengerId == row.PassengerId), 'family_survived'] = 1
+
+train_df = pd.get_dummies(train_df)
+x = train_df.iloc[:, 2:]
+y = train_df.iloc[:, 1]
+clf = DecisionTreeClassifier()
+clf2 = RandomForestClassifier()
+clf3 = SVC(kernel='linear', C=10)
+clf4 = LogisticRegression(max_iter=5000, C=0.2)
+vlf = VotingClassifier([('clf1', clf), ('clf2', clf2), ('clf3', clf3), ('clf4', clf4)])
+sc = model_selection.cross_val_score(clf3, x, y, scoring='accuracy')
+clf3.fit(x, y)
+print(sc.mean())
+print(clf3.score(x, y))
